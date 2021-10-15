1616e9cef3bf39dc9c83757e8a3c20cdac18ebe7
Chase Patterson <76974505+Helix088@users.noreply.github.com>
2021-10-14 18:41:41 -0600

    Update Main.java
    Hello!

--- src/smith/Main.java
+++ src/smith/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Brian");
         System.out.println("Hello Brian, my name is Paul");
+        System.out.println("Hello Brain, my name is Chase");
     }
 }
