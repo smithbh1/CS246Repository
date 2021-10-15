f32873fa6271d5a8da3e4989a0ba3766ce22a920
Rex-1nvictus <and13005@byui.edu>
2021-10-14 18:23:11 -0600

    Add my own message

--- src/smith/Main.java
+++ src/smith/Main.java
@@ -3,6 +3,7 @@ package smith;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Brian");
+        System.out.println("Hello, my name is Brian");
+        System.out.println("Hello Brian, my name is Paul");
     }
 }
